ea8c987375771753fc4c5a808243d7f73e0911f8
Anoopjain8 <anoopjain8@gmail.com>
2024-08-29 23:55:15 +0530

    create custom file

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// import {jsx as _jsx} from 'react/jsx-runtime'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+const anotherElement = 'Anoop'
+const reactElement = React.createElement(
+    'a',
+    {href: 'http://google.com',target: '_blank'},
+    'click me to visit google',
+    anotherElement
+)
+ 
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <App />
+    reactElement
 )
--- vite-project/src/try.jsx
+++ vite-project/src/try.jsx
@@ -1,6 +1,7 @@
 function Try(){
+    const text = 'hello world'
     return(
-        <h2>first function</h2>
+        <h2>first function {text}</h2>
     )
 }
 export default Try
